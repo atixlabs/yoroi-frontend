6456fc1c2d71a51346546f6c86e1692071ead62e
Nicolas Arqueros <nykholas@gmail.com>
2018-11-23 01:39:27 -0500

    hot reloading working

--- app/App.js
+++ app/App.js
@@ -15,12 +15,13 @@ import translations from './i18n/translations';
 import type { StoresMap } from './stores/index';
 import type { ActionsMap } from './actions/index';
 import ThemeManager from './ThemeManager';
+import { hot } from 'react-hot-loader'
 
 // https://github.com/yahoo/react-intl/wiki#loading-locale-data
 addLocaleData([...en, ...ko, ...ja, ...zh, ...ru]);
 
 @observer
-export default class App extends Component<{
+class App extends Component<{
   stores: StoresMap,
   actions: ActionsMap,
   history: Object,
@@ -54,3 +55,5 @@ export default class App extends Component<{
     );
   }
 }
+
+export default hot(module)(App)
