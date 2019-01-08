40a3d149d783b151bec186aa5003a1f304b1816c
Nicole Tobal <ntobal@atixlabs.com>
2019-01-08 12:54:07 -0300

    [PI-21] Regular redemption form started

--- app/containers/wallet/AdaRedemptionPage.js
+++ app/containers/wallet/AdaRedemptionPage.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import type { InjectedProps } from '../../types/injectedPropsType';
 import AdaRedemptionChoices from '../../components/wallet/ada-redemption/AdaRedemptionChoices';
-import type { RedemptionTypeChoices } from '../../types/redemptionTypes';
+import RegularAdaRedemption from '../../components/wallet/ada-redemption/RegularAdaRedemption';
+import { ADA_REDEMPTION_TYPES } from '../../types/redemptionTypes';
 
 @inject('stores', 'actions') @observer
 export default class AdaRedemptionPage extends Component<InjectedProps> {
@@ -12,8 +13,15 @@ export default class AdaRedemptionPage extends Component<InjectedProps> {
   render() {
     const { stores, actions } = this.props;
 
-    const { redemptionType } = stores.adaRedemption;
-    const { chooseRedemptionType } = actions.ada.adaRedemption;
+    const {
+      redeemAdaRequest, redeemPaperVendedAdaRequest, isCertificateEncrypted, isValidRedemptionKey,
+      redemptionType, isValidRedemptionMnemonic, isValidPaperVendRedemptionKey,
+      isRedemptionDisclaimerAccepted, error
+    } = stores.adaRedemption;
+    const {
+      chooseRedemptionType, setCertificate, setPassPhrase, setRedemptionCode, removeCertificate,
+      setEmail, setAdaPasscode, setAdaAmount, setDecryptionKey, acceptRedemptionDisclaimer
+    } = actions.ada.adaRedemption;
 
     const onChooseRedemptionType = (choice) =>
       chooseRedemptionType.trigger({ redemptionType: choice });
@@ -27,6 +35,44 @@ export default class AdaRedemptionPage extends Component<InjectedProps> {
             /* TODO: reset form */
             onChooseRedemptionType(choice);
           }}
+        />
+        <RegularAdaRedemption
+          onCertificateSelected={(certificate) => setCertificate.trigger({ certificate })}
+          onPassPhraseChanged={(passPhrase) => setPassPhrase.trigger({ passPhrase })}
+          onRedemptionCodeChanged={(redemptionCode) => {
+            setRedemptionCode.trigger({ redemptionCode });
+          }}
+          onEmailChanged={(email) => setEmail.trigger({ email })}
+          onAdaAmountChanged={(adaAmount) => setAdaAmount.trigger({ adaAmount })}
+          onAdaPasscodeChanged={(adaPasscode) => setAdaPasscode.trigger({ adaPasscode })}
+          onDecryptionKeyChanged={(decryptionKey) => setDecryptionKey.trigger({ decryptionKey })}
+          onChooseRedemptionType={(choice) => {
+            chooseRedemptionType.trigger({ redemptionType: choice });
+          }}
+          redemptionCode={stores.adaRedemption.redemptionCode}
+          // wallets={selectableWallets}
+          // suggestedMnemonics={validWords}
+          // isCertificateSelected={isCertificateSelected}
+          isCertificateEncrypted={isCertificateEncrypted}
+          // isCertificateInvalid={error instanceof AdaRedemptionCertificateParseError}
+          // isSubmitting={request.isExecuting}
+          error={stores.adaRedemption.error}
+          onRemoveCertificate={removeCertificate.trigger}
+          onSubmit={(redemptionType === ADA_REDEMPTION_TYPES.PAPER_VENDED ?
+            this.onSubmitPaperVended : this.onSubmit
+          )}
+          mnemonicValidator={isValidRedemptionMnemonic}
+          redemptionCodeValidator={isValidRedemptionKey}
+          postVendRedemptionCodeValidator={isValidPaperVendRedemptionKey}
+          redemptionType={redemptionType}
+          // showInputsForDecryptingForceVendedCertificate={
+          //   showInputsForDecryptingForceVendedCertificate
+          // }
+          // showInputForDecryptionKey={showInputForDecryptionKey}
+          // showPassPhraseWidget={showPassPhraseWidget}
+          isRedemptionDisclaimerAccepted={isRedemptionDisclaimerAccepted}
+          onAcceptRedemptionDisclaimer={() => acceptRedemptionDisclaimer.trigger()}
+          getSelectedWallet={() => ({})}
         />
       </div>
     );
