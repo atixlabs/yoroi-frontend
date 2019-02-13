1d09e8da69c522ad93ba604a76a935982319e4f6
Nicole Tobal <ntobal@atixlabs.com>
2019-02-13 17:54:07 -0300

    [PI-79] Comments added to explain the redeemAda function's behaviour

--- app/api/ada/adaRedemption.js
+++ app/api/ada/adaRedemption.js
@@ -24,18 +24,23 @@ export type RedeemPaperVendedAdaParams = {
 async function createAndSendTx(
   redemptionKey: Buffer
 ) : Promise<BigNumber> {
+  // The address associated to the redemption key is obtained
   const uint8ArrayAddress = getAddressFromRedemptionKey(redemptionKey);
   const senderAddress = bs58.encode(Buffer.from(uint8ArrayAddress));
+  // The UTXO belonging to the certificate's address is fetched
   const utxos = await getUTXOsForAddresses({ addresses: [senderAddress] });
   if (utxos.length === 0) {
     throw new RedemptionKeyAlreadyUsedError();
   }
   const receiverAddress = await getReceiverAddress();
+  // Ada Redemption Signed Transaction is created with the redemption key and the UTXO
   const redemptionSignedTransaction: RedeemResponse =
     getRedemptionSignedTransaction(redemptionKey, receiverAddress, utxos[0]);
   const cborEncodedTx = redemptionSignedTransaction.result.cbor_encoded_tx;
   const signedTx = Buffer.from(cborEncodedTx).toString('base64');
+  // Transaction is broadcasted
   await sendTx({ signedTx });
+  // If the broadcast didn't fail, the UTXO's amount is returned
   return new BigNumber(utxos[0].amount);
 }
 
@@ -50,6 +55,7 @@ export async function redeemAda(
 export async function redeemPaperVendedAda(
   redemptionParams: RedeemPaperVendedAdaParams
 ) : Promise<BigNumber> {
+  // Redemption key is obtained by decrypting the redemption code with the mnemonics
   const redemptionCodeBuffer = bs58.decode(redemptionParams.redemptionCode);
   const mnemonicAsString = redemptionParams.mnemonics.join(' ');
   const seed = decryptRegularVend(mnemonicAsString, redemptionCodeBuffer);
